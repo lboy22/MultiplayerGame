acd00d674cbde3acdd0dd25398bcf824da522660
lboy22 <44554960+lboy22@users.noreply.github.com>
2024-03-20 02:26:23 -0700

    Added Sound effects and fixed movement

--- Multiplayeragme/Assets/Scripts/PlayerScript.cs
+++ Multiplayeragme/Assets/Scripts/PlayerScript.cs
@@ -1,94 +1,162 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    [SerializeField] float moveHorizontal, speed = 4f, bulletVelocity = 5f;
-    [SerializeField] bool shooting = false;
-    Rigidbody playerBody;
-    Animator animator;
-    public GameObject bullet1;
-    [SerializeField] ProjectileBehaviour ProjectilePrefab;
-    public Transform LaunchOffSet;
+    public float walkSpeed = 2.5f;
+    public float jumpHeight = 5f;
 
-    // Start is called before the first frame update
+    public Transform groundCheckTransform;
+    public float groundCheckRadius = 0.2f;
+
+    public Transform targetTransform;
+    public LayerMask mouseAimMask;
+    public LayerMask groundMask;
+
+    public GameObject bulletPrefab;
+    public Transform muzzleTransform;
+
+    public AnimationCurve recoilCurve;
+    public float recoilDuration = 0.25f;
+    public float recoilMaxRotation = 45f;
+    public Transform rightLowerArm;
+    public Transform rightHand;
+
+    private float inputMovement;
+    private Animator animator;
+    private Rigidbody rbody;
+    private bool isGrounded;
+    private Camera mainCamera;
+    private float recoilTimer;
+
+    [SerializeField] float footstepDelay = 0.35f, lastFootstepDelay = 0f;
+    [SerializeField] AudioClip footstepSound, gunshot, ready, jump, tired, deathSound;
+    [SerializeField] AudioSource audioSource, gunShotSource, jumpSource, tiredSource, readySource, deathSource;
+
+    private int FacingSign
+    {
+        get
+        {
+            Vector3 perp = Vector3.Cross(transform.forward, Vector3.forward);
+            float dir = Vector3.Dot(perp, transform.up);
+            return dir > 0f ? -1 : dir < 0f ? 1 : 0;
+        }
+    }
+
     void Start()
     {
-        playerBody = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        if(rbody.velocity.x > 0)
+        {
+            readySource.PlayOneShot(ready);
+        }
+        inputMovement = Input.GetAxis("Horizontal");
 
-    }
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-    void FixedUpdate()
-    {
-        Movement();
-        Shooting();
-    }
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, mouseAimMask))
+        {
+            targetTransform.position = hit.point;
+        }
 
-    void Movement()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        playerBody.velocity = new Vector2(moveHorizontal * speed, playerBody.velocity.y);
-        CheckAnimation();
-    }
-
-    void Shooting()
-    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rbody.velocity = new Vector3(rbody.velocity.x, 0, 0);
+            rbody.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -1 * Physics.gravity.y), ForceMode.VelocityChange);
+            jumpSource.PlayOneShot(jump);
+        }
+        else if(Input.GetButtonDown("Jump") && !isGrounded)
+        {
+            tiredSource.PlayOneShot(tired);
+        }
 
         if (Input.GetButtonDown("Fire1"))
         {
-            shooting = true;
-            Instantiate(ProjectilePrefab, LaunchOffSet.position, transform.rotation);
+            Fire();
         }
-
-        CheckAnimation();
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            DeathTest();
+        }
     }
 
-    void CheckAnimation()
+    private void Fire()
     {
-        if(speed > 0f)
-        {
-            animator.SetFloat("speed", playerBody.velocity.x / speed);
-        }
-        else if(speed < 0f)
-        {
+        recoilTimer = Time.time;
 
-        }
-        if (shooting)
+        var go = Instantiate(bulletPrefab);
+        gunShotSource.PlayOneShot(gunshot);
+        go.transform.position = muzzleTransform.position;
+        var bullet = go.GetComponent<Bullet>();
+        bullet.Fire(go.transform.position, muzzleTransform.eulerAngles, gameObject.layer);
+    }
+
+    private void LateUpdate()
+    {
+        // Recoil Animation
+        if (recoilTimer < 0)
         {
-            animator.SetBool("shooting", true);
-            // Reset the Animator's shooting boolean after a short delay or at the end of the animation.
-            StartCoroutine(ResetShootingAnimation());
-            shooting = false;
+            return;
+        }
+
+        float curveTime = (Time.time - recoilTimer) / recoilDuration;
+        if (curveTime > 1f)
+        {
+            recoilTimer = -1;
         }
         else
         {
-            animator.SetBool("shooting", false);
+            rightLowerArm.Rotate(Vector3.forward, recoilCurve.Evaluate(curveTime) * recoilMaxRotation, Space.Self);
         }
+
+
     }
 
-    IEnumerator ResetShootingAnimation()
+    private void FixedUpdate()
     {
-        // Wait for a bit before resetting the shooting animation.
-        // Adjust the wait time based on the length of your shooting animation.
-        yield return new WaitForSeconds(0.5f); // Example: 0.5 seconds
-        animator.SetBool("shooting", false);
-    }
+        // Movement
+        rbody.velocity = new Vector3(inputMovement * walkSpeed, rbody.velocity.y, 0);
+        animator.SetFloat("speed", (FacingSign * rbody.velocity.x) / walkSpeed);
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.tag == "Player")
+        // Facing Rotation
+        rbody.MoveRotation(Quaternion.Euler(new Vector3(0, 90 * Mathf.Sign(targetTransform.position.x - transform.position.x), 0)));
+
+        // Ground Check
+        isGrounded = Physics.CheckSphere(groundCheckTransform.position, groundCheckRadius, groundMask, QueryTriggerInteraction.Ignore);
+        animator.SetBool("isGrounded", isGrounded);
+
+        // Play footstep sound if moving on x-axis and enough time has passed since the last footstep sound
+        if (Mathf.Abs(inputMovement) > 0.1f && Time.time - lastFootstepDelay > footstepDelay)
         {
-            Debug.Log("" + collision.gameObject.name);
-            animator.SetBool("hit", true);
-            Destroy(collision.gameObject);
+            audioSource.PlayOneShot(footstepSound);
+            lastFootstepDelay = Time.time;
         }
+    }
 
+    private void OnAnimatorIK()
+    {
+        // Weapon Aim at Target IK
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+        animator.SetIKPosition(AvatarIKGoal.RightHand, targetTransform.position);
+
+        // Look at target IK
+        animator.SetLookAtWeight(1);
+        animator.SetLookAtPosition(targetTransform.position);
+    }
+
+    void DeathTest()
+    {
+        deathSource.PlayOneShot(deathSound);
+        Destroy(this);
     }
 }
